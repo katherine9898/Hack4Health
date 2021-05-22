c3bd995e83e8a626b5b9798d03ac6d3268d6b6d8
Joonior-Programmer <kkjh6457@gmail.com>
2021-05-22 13:41:34 -0700

    finished backend setup

--- Backend/app.js
+++ Backend/app.js
@@ -1,41 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const userRouter = require("./routes/user");
+var hospitalRouter = require("./routes/hospital");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+app.set("trust proxy", 1);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Session settings
+app.use(
+  session({
+    secret: "jIPEyvTVBGPu7vfA",
+    resave: true,
+    rolling: true,
+    saveUninitialized: false,
+    cookie: {
+      //secure: true,
+      maxAge: 1000 * 60 * 15, //session expires in 15 minutes
+    },
+  })
+);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Allow crossed domain requests
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  //res.header("Content-Type", "application/json;charset=utf-8");
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use("/hospital", hospitalRouter);
+app.use("/user", userRouter);
 
 module.exports = app;
