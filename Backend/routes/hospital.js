dce7d457f408339bb6de90ccebd720cb397ebe8c
Min Soo HWANG <sis00337@gmail.com>
2021-05-22 19:42:23 -0700

    Search hospital by name

--- Backend/routes/hospital.js
+++ Backend/routes/hospital.js
@@ -4,11 +4,12 @@ const database = require("../common/database");
 const utils = require("../common/utils");
 
 
-/* GET home page. */
-router.post("/hospital", function (req, res, next) {
+// Search by Name
+router.post("/hospital", async function (req, res, next) {
   try {
-    let SQL = "SELECT hospital_id, hospital_name, hospital_latitude, hospital_longitude, hospital_is_available_for_vaccine FROM hospital"
-    let result = await database.QueryMySQL(SQL);
+    params = [req.body.hospitalName.toLowerCase()]
+    let SQL = "SELECT hospital_id, hospital_name, hospital_latitude, hospital_longitude, hospital_is_available_for_vaccine FROM hospital WHERE hospital_name LIKE '%?%'"
+    let result = await database.QueryMySQL(SQL, params);
     return utils.SendResult(res, result);
   }
   catch (err) {
@@ -16,9 +17,5 @@ router.post("/hospital", function (req, res, next) {
   }
 });
 
-router.post("/find", auth, async function (req, res, next) {
-  let location = req.body.location;
-  let distance = req.body.distance;
-})
 
 module.exports = router;
