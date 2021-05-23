db442b1912cf62b1bec9f948f217dfeedef1acbc
Sijan <sijuichi1@gmail.com>
2021-05-22 19:58:15 -0700

    Landing page

--- Frontend/src/components/user/Main.vue
+++ Frontend/src/components/user/Main.vue
@@ -1,13 +1,20 @@
 <template>
-  <div id="">
-    <van-button type="gh" text="Hi" color="black">Warning</van-button>
-    <van-rate
-      v-model="value"
-      :size="25"
-      color="#ffd21e"
-      void-icon="star"
-      void-color="#eee"
-    />
+  <div class= "main-container" >
+
+  <div class="header">
+ 
+<van-row type="flex" justify="end">
+ <van-col class="logo" span="10">Hopify</van-col>
+ <van-col span="3"> <button>About Us</button></van-col>
+ <van-col span="3"><button>Home</button> </van-col>
+<van-col span="3"> <button>Contact Us</button></van-col>
+</van-row>
+  </div>
+    
+    <div class ="body">
+      <p1> Landing page </p1>
+    </div>
+ 
   </div>
 </template>
 
@@ -18,6 +25,7 @@ export default {
     return {
       number: 0,
       cellphone: "IPhone",
+      val: "Name"
     };
   },
   mounted() {},
@@ -26,8 +34,55 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Chela+One&display=swap');
+
 #main_tab_view {
   width: 100%;
   height: 100%;
 }
+
+.logo{
+   font-family: 'Chela One', cursive;
+   font-size: 30px;
+   padding: 15px 15px;
+  }
+
+.header{
+  background-color: #C5D5DE;
+  height: 1cm;
+  width: 100%;
+  padding: 0.5cm;
+  list-style-type: none;
+  font-size: 0.5cm;
+
+  }
+
+  li{
+     margin: 0.5cm;
+     padding: 0.5cm;
+    
+     }
+
+  button{
+       border: none;
+       color: black;
+       background-color: #688895;
+       padding: 15px 15px;
+       border-radius: 8px;
+
+       }
+
+  
+.main-container{
+  background-color: #C7E9F3;
+  height: 100vh;
+  width: 100%
+  
+  }
+
+  .body{
+     padding: 50px;
+  }
+
+
 </style>
