c73ac04674d9914f1abc7fdc1ae76bab6f7c5c7d
Min Soo HWANG <sis00337@gmail.com>
2021-05-22 18:41:34 -0700

    Connected to the database.

--- Backend/routes/hospital.js
+++ Backend/routes/hospital.js
@@ -1,9 +1,24 @@
 var express = require("express");
 var router = express.Router();
+const database = require("../common/database");
+const utils = require("../common/utils");
+
 
 /* GET home page. */
-router.get("/hospital", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.post("/hospital", function (req, res, next) {
+  try {
+    let SQL = "SELECT hospital_id, hospital_name, hospital_latitude, hospital_longitude, hospital_is_available_for_vaccine FROM hospital"
+    let result = await database.QueryMySQL(SQL);
+    return utils.SendResult(res, result);
+  }
+  catch (err) {
+    utils.SendError(res, err);
+  }
 });
+
+router.post("/find", auth, async function (req, res, next) {
+  let location = req.body.location;
+  let distance = req.body.distance;
+})
 
 module.exports = router;
